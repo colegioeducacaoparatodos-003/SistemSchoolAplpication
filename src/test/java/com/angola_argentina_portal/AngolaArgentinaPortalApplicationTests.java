5c9fce8d69b37658cc820695136365ad8bb112e7
Jacirosi <jacirosidev@gmail.com>
2026-02-20 13:35:20 +0100

    Initial commit

---
+++ src/test/java/com/angola_argentina_portal/AngolaArgentinaPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angola_argentina_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngolaArgentinaPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
