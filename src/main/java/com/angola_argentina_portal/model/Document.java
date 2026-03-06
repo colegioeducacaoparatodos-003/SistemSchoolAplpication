6f10ecb733272a7d02bacc8c69651fc022f16e7d
ChipungoJasiRosi <chipungojaci@gmail.com>
2026-03-06 16:32:34 +0100

    Implement an MVC structure for the documents.

---
+++ src/main/java/com/angola_argentina_portal/model/Document.java
@@ -0,0 +1,118 @@
+package com.angola_argentina_portal.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "documents")
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String referenceType;
+    //ASSENT | CONTRACT | EMPLOYER | PAYROLL | OTHER
+
+    private String referenceId;
+
+    private String documentType;
+    // CONTRACT | INVOICE | MANUAL | PHOTO | REPORT
+
+    private String fileName;
+    private String filePath;
+    private String contentType;
+
+    private Long fileSize;
+
+    private LocalDate uploadDate;
+
+    private int fkUser;
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReferenceType() {
+        return referenceType;
+    }
+
+    public void setReferenceType(String referenceType) {
+        this.referenceType = referenceType;
+    }
+
+    public String getReferenceId() {
+        return referenceId;
+    }
+
+    public void setReferenceId(String referenceId) {
+        this.referenceId = referenceId;
+    }
+
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public LocalDate getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(LocalDate uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    public int getFkUser() {
+        return fkUser;
+    }
+
+    public void setFkUser(int fkUser) {
+        this.fkUser = fkUser;
+    }
+
+}
