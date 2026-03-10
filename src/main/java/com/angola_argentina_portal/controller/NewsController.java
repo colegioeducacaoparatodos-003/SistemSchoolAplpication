6f715f5981e3a6c1fb89fc859eccc82cecc2e9dd
Paulino Bambuca <paulinogoncalves2022@gmail.com>
2026-03-10 18:27:24 +0100

    News

--- src/main/java/com/angola_argentina_portal/controller/NewsController.java
+++ src/main/java/com/angola_argentina_portal/controller/NewsController.java
@@ -1,12 +1,12 @@
 package com.angola_argentina_portal.controller;
 import java.util.List;
 
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import com.angola_argentina_portal.dto.NewsDTO;
 import com.angola_argentina_portal.dto.NewsStatus;
@@ -20,27 +20,11 @@ public class NewsController {
     private NewService newService;
 
     // Retorna todas as notícias
-    @GetMapping("/all")
-    public ResponseEntity<List<NewsDTO>> getAllNews() {
-        return ResponseEntity.ok(newService.getAllNews());
+    public void ListarNews(){
+        try {
+            List<NewsDTO> newsList = newService.getAllNews();
+        } catch (Exception e) {
+            
+        }
     }
-
-    // Notícias por status
-    @GetMapping
-    public ResponseEntity<List<NewsDTO>> getNewsByStatus(@PathVariable NewsStatus status) {
-        return ResponseEntity.ok(newService.getNewsByStatus(status));
-    }
-
-    // Pesquisar notícias pelo título
-    @GetMapping("/search")
-    public ResponseEntity<List<NewsDTO>> searchNewsByTitle(@RequestParam("q") String keyword) {
-        return ResponseEntity.ok(newService.searchNewsByTitle(keyword));
-    }
-
-    // Top notícias por visualizações
-    @GetMapping("/top")
-    public ResponseEntity<List<NewsDTO>> getTopNewsByViews() {
-        return ResponseEntity.ok(newService.getTopNewsByViews());
-    }
-
 }
