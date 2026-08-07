7929a4c2acc7c2c8d63123281e702c9600ec173e
Paulino Gonçalves Bambuca <paulinogoncalvesb@gmail.com>
2026-08-07 22:58:06 +0100

    fix: altera para WAR, remove Spring Security, mantem estrutura webapp

--- src/main/java/com/SistemSchool/config/JsfConfig.java
+++ src/main/java/com/SistemSchool/config/JsfConfig.java
@@ -1,9 +1,12 @@
 package com.SistemSchool.config;
 
 import jakarta.faces.webapp.FacesServlet;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,21 +14,24 @@ import org.springframework.context.annotation.Configuration;
 import java.io.IOException;
 
 @Configuration
-public class JsfConfig {
+public class JsfConfig implements ServletContextInitializer {
 
-    @Bean
-    public ServletRegistrationBean<FacesServlet> facesServlet() {
-        ServletRegistrationBean<FacesServlet> registration = new ServletRegistrationBean<>();
-        registration.setServlet(new FacesServlet());
-        registration.addUrlMappings("*.xhtml", "*.jsf");
-        registration.setLoadOnStartup(1);
-        registration.setName("FacesServlet");
-        return registration;
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.setInitParameter("jakarta.faces.DEFAULT_SUFFIX", ".xhtml");
+        servletContext.setInitParameter("jakarta.faces.PROJECT_STAGE", "Production");
+        servletContext.setInitParameter("primefaces.THEME", "saga");
+        servletContext.setInitParameter("org.apache.myfaces.NUMBER_OF_VIEWS_IN_SESSION", "25");
+        servletContext.setInitParameter("org.apache.myfaces.NUMBER_OF_SEQUENTIAL_VIEWS_IN_SESSION", "5");
+
+        var facesServlet = servletContext.addServlet("FacesServlet", FacesServlet.class);
+        facesServlet.addMapping("*.xhtml", "*.jsf");
+        facesServlet.setLoadOnStartup(1);
     }
 
     @Bean
-    public ServletRegistrationBean<HttpServlet> rootRedirectServlet() {
-        ServletRegistrationBean<HttpServlet> registration = new ServletRegistrationBean<>();
+    public ServletRegistrationBean<HttpServlet> rootRedirect() {
+        var registration = new ServletRegistrationBean<HttpServlet>();
         registration.setServlet(new HttpServlet() {
             @Override
             protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -33,7 +39,6 @@ public class JsfConfig {
             }
         });
         registration.addUrlMappings("/");
-        registration.setName("RootRedirect");
         registration.setLoadOnStartup(2);
         return registration;
     }
