286591f1abf6d2828e2c1353d631a8dcf5427b7f
Paulino Gonçalves Bambuca <paulinogoncalvesb@gmail.com>
2026-08-08 16:15:55 +0100

    fix: target\SistemSchool-0.0.1-SNAPSHOT.jar

--- src/main/java/com/SistemSchool/config/TomcatJsfCustomizer.java
+++ src/main/java/com/SistemSchool/config/TomcatJsfCustomizer.java
@@ -1,5 +1,25 @@
 package com.SistemSchool.config;
 
+import jakarta.faces.webapp.FacesServlet;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class TomcatJsfCustomizer {
 
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> jsfServletCustomizer() {
+        return factory -> factory.addInitializers(servletContext -> {
+            System.out.println(">>> TOMCAT CUSTOMIZER: Registrando FacesServlet <<<");
+            
+            jakarta.servlet.ServletRegistration.Dynamic facesServlet = 
+                servletContext.addServlet("FacesServlet", FacesServlet.class);
+            facesServlet.addMapping("*.xhtml", "*.jsf");
+            facesServlet.setLoadOnStartup(1);
+            
+            System.out.println(">>> TOMCAT CUSTOMIZER: FacesServlet registrado <<<");
+        });
+    }
 }
